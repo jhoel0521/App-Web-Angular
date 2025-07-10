090cb4b71d96e28508ac752e0509b6dfa4cedbcf
jhoel0521 <jhoel0521@gmail.com>
2025-07-09 20:43:13 -0400

    add 2 paginas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AcercaComponent } from './pages/acerca/acerca.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'acerca', component: AcercaComponent },
+    { path: '**', redirectTo: '' }
+];
