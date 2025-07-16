aa055867aee09b8778ed0f020ea605b3a67e6c11
Jhoel <32343423+jhoel0521@users.noreply.github.com>
2025-07-16 00:50:37 +0000

    mejorar la estructura y legibilidad del componente Cardbuscador y corregir la lógica de visualización de productos

--- src/app/components/cardbuscador/cardbuscador.component.ts
+++ src/app/components/cardbuscador/cardbuscador.component.ts
@@ -2,12 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ProductoService } from '../../services/productos.service';
 import { Producto } from '../../models/producto.modelds';
-import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-cardbuscador',
-  imports: [CommonModule, RouterModule],
+  imports: [RouterModule],
   templateUrl: './cardbuscador.component.html',
   styleUrl: './cardbuscador.component.css'
 })
